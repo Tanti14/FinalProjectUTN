5946f47be40b2375af30e2cca966ed5e89e6e360
Tanti14 <santicostanti@gmail.com>
2023-12-08 17:13:20 -0300

    feat:added login and logout functions

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -39,6 +39,47 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const foundUser = await User.findOne({ email });
+
+    if (!foundUser)
+      return res.status(400).json({ message: "Usuario no encontrado" });
+
+    const isMatch = await bcrypt.compare(password, foundUser.password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Credenciales Incorrectas" });
+
+    const token = await createAccessToken({ id: foundUser._id });
+    /* createAccessToken viene de libs/jwt.js */
+
+    res.cookie(
+      "token",
+      token
+    ); /* Seteamos una cookie llamada token con el valor del token creado con jwt */
+
+    res.json({
+      id: foundUser._id,
+      nombre: foundUser.nombre,
+      apellido: foundUser.apellido,
+      phone: foundUser.phone,
+      email: foundUser.email,
+      createdAt: foundUser.createdAt,
+      updatedAt: foundUser.updatedAt,
+    });
+  } catch (error) {
+    console.log("Ha ocurrido un error:", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+
+  return res.sendStatus(200);
 };
